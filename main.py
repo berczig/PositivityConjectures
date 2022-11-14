27fa02c51580e14da36aaaf2ad33a3f1dfc8c09e
HF17 <nasytw@gmail.com>
2022-11-14 13:06:31 +0100

    added main

---
+++ main.py
@@ -0,0 +1,31 @@
+from Datahandler import *
+from LinearTransformer import *
+import matplotlib.pyplot as plt
+
+trainX, testX, trainY, testY = loadxlsx("chrom53.xlsx", 0.8)
+
+plt.plot(trainY[:, 0], "g", trainY[:, 1], "r")
+plt.xlabel("Graph ID")
+plt.legend(["only l3 correct", "only l+3 correct"])
+plt.show()
+
+LT = LinearTransformer(trainX, trainY, normed=True)
+print("Insample error:", LT.getScore(trainX, trainY))
+print("Outsample error:", LT.getScore(testX, testY))
+
+Yhat = LT.map(trainX)
+coeffs = trainY[:, 0]-trainY[:, 1]
+coeffs_pred = Yhat[:, 0] - Yhat[:, 1]
+plt.plot(coeffs, "g", coeffs_pred, "r")
+plt.xlabel("Graph ID")
+plt.legend(["real coeffs", "predicted coeffs(insample)"])
+plt.show()
+
+
+Yhat = LT.map(testX)
+coeffs = testY[:, 0]-testY[:, 1]
+coeffs_pred = Yhat[:, 0] - Yhat[:, 1]
+plt.plot(coeffs, "g", coeffs_pred, "r")
+plt.xlabel("Graph ID")
+plt.legend(["real coeffs", "predicted coeffs(outsample)"])
+plt.show()
