7e15a2cd520d8a0c0d9c95b9c130c85a038d362d
au596283 <gergely.berczi@gmail.com>
2024-09-04 14:23:20 +0200

    Quadruple cores and comparisons fixed

--- SPC/Restructure/cores/EscherCoreGeneratorQuadruple.py
+++ SPC/Restructure/cores/EscherCoreGeneratorQuadruple.py
@@ -16,19 +16,28 @@ class EscherCoreGeneratorQuadruple(EscherCoreGeneratorAbstract):
         return {
             "0" : ["subescher uv start", "subescher uw start", "subescher vw start", "subescher uv_w start", "subescher uw_v start", "subescher vw_u start", 
                    "subescher uz start", "subescher vz start", "subescher wz start", "subescher uz_w start", "subescher vz_u start", "subescher wz_v start"],
-            "subescher uv end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert"],
-            "subescher uw end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert"],
-            "subescher vw end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert"],
-            "subescher uv_w end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert","uw 1. insert","vw 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
-            "subescher uw_v end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert","uv 1. insert","vw 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
-            "subescher vw_u end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv 1. insert","uw 1. insert", "uz_w 1. insert", "vz_u 1. insert","wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
-            "subescher uz end" : ["uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"],
-            "subescher vz end" : ["uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"],
-            "subescher wz end" : ["uz 1. insert", "vz 1. insert", "wz 1. insert", "uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"],
-            "subescher uz_w end" : ["uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"],
-            "subescher vz_u end" : ["uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"],
-            "subescher wz_v end" : ["uz_w 1. insert", "vz_u 1. insert", "wz_v 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert", "uv 1. insert", "uw 1. insert", "vw 1. insert", "uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert"]}
-            
+            "subescher uv end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher uw end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher vw end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher uz end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher vz end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher wz end" : ["uv 1. insert","uw 1. insert","vw 1. insert", "uz 1. insert", "vz 1. insert", "wz 1. insert"],
+            "subescher uv_w end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uw_v end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher vw_u end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uv_z end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uz_v end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher vz_u end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uw_z end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uz_w end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher wz_u end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher vw_z end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher vz_w end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher wz_v end" : ["uv_w 1. insert", "uw_v 1. insert", "vw_u 1. insert", "uv_z 1. insert", "uz_v 1. insert", "vz_u 1. insert","uw_z 1. insert", "uz_w 1. insert", "wz_u 1. insert", "vw_z 1. insert", "vz_w 1. insert", "wz_v 1. insert","uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uvw_z end" : ["uvw_z 1. insert", "uvz_w 1. insert", "uwz_v 1. insert", "vwz_u 1. insert", "uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uvz_w end" : ["uvw_z 1. insert", "uvz_w 1. insert", "uwz_v 1. insert", "vwz_u 1. insert", "uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher uwz_v end" : ["uvw_z 1. insert", "uvz_w 1. insert", "uwz_v 1. insert", "vwz_u 1. insert", "uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],
+            "subescher vwz_u end" : ["uvw_z 1. insert", "uvz_w 1. insert", "uwz_v 1. insert", "vwz_u 1. insert", "uv_wz 1. insert", "uw_vz 1. insert", "vw_uz 1. insert"],}
 
 
     def getCoreLabels(partition):
@@ -42,11 +51,25 @@ class EscherCoreGeneratorQuadruple(EscherCoreGeneratorAbstract):
         return ["0", "subescher uv start", "subescher uv end", "uv 1. insert", "subescher vw start", "subescher vw end", "vw 1. insert", 
                 "subescher uw start", "subescher uw end", "uw 1. insert", "subescher uz start", "subescher uz end", "uz 1. insert",
                 "subescher vz start", "subescher vz end", "vz 1. insert", "subescher wz start", "subescher wz end", "wz 1. insert",
-                "subescher uv_w start", "subescher uv_w end", "uv_w 1. insert", "subescher uw_v start", "subescher uw_v end", "uw_v 1. insert",
-                "subescher vw_u start", "subescher vw_u end", "vw_u 1. insert", "subescher uz_w start", "subescher uz_w end", "uz_w 1. insert",
-                "subescher vz_u start", "subescher vz_u end", "vz_u 1. insert", "subescher wz_v start", "subescher wz_v end", "wz_v 1. insert"]
-    
-    #@staticmethod
+                "subescher uv_w start", "subescher uv_w end", "uv_w 1. insert", 
+                "subescher uw_v start", "subescher uw_v end", "uw_v 1. insert",
+                "subescher vw_u start", "subescher vw_u end", "vw_u 1. insert", 
+                "subescher uv_z start", "subescher uv_z end", "uv_z 1. insert",
+                "subescher uz_v start", "subescher uz_v end", "uz_v 1. insert",
+                "subescher vz_u start", "subescher vz_u end", "vz_u 1. insert",
+                "subescher uw_z start", "subescher uw_z end", "uw_z 1. insert", 
+                "subescher uz_w start", "subescher uz_w end", "uz_w 1. insert",
+                "subescher wz_u start", "subescher wz_u end", "wz_u 1. insert",
+                "subescher vw_z start", "subescher vw_z end", "vw_z 1. insert",
+                "subescher vz_w start", "subescher vz_w end", "vz_w 1. insert",
+                "subescher wz_v start", "subescher wz_v end", "wz_v 1. insert",
+                "subescher uvw_z start", "subescher uvw_z end", "uvw_z 1. insert", 
+                "subescher uvz_w start", "subescher uvz_w end", "uvz_w 1. insert",
+                "subescher uwz_v start", "subescher uwz_v end", "uwz_v 1. insert", 
+                "subescher vwz_u start", "subescher vwz_u end", "vwz_u 1. insert",
+                "subescher uv_wz start", "subescher uv_wz end", "uv_wz 1. insert", 
+                "subescher uw_vz start", "subescher uw_vz end", "uw_vz 1. insert",
+                "subescher vw_uz start", "subescher vw_uz end", "vw_uz 1. insert",]
     #def getCoreLabels(partition):
     #    return ["a", "b", "c"]
 
@@ -67,35 +90,48 @@ class EscherCoreGeneratorQuadruple(EscherCoreGeneratorAbstract):
         core_w_z = self.get_shortb_insertion_and_subescher_of_2_eschers(w,z)
 
         
-        # If there is no insertion, then it is good
-        #if core_u_v == [] or core_u_w == [] or core_v_w == []:
-        #    return "GOOD"
+        # Calculate the core of a triple pair and ab_cd
 
         if core_u_v[-1] == -1:
             core_uv_w = [-1, -1, -1]
             core_uv_z = [-1, -1, -1]
+            core_uv_wz = [-1, -1, -1]
         else:
             uv = self.concat(u, v, insertionpoint=core_u_v[-1])
             core_uv_w = self.get_shortb_insertion_and_subescher_of_2_eschers(uv,w)
             core_uv_z = self.get_shortb_insertion_and_subescher_of_2_eschers(uv,z)
+            if core_w_z[-1] == -1:
+                core_uv_wz = [-1, -1, -1]
+            else:
+                core_uv_wz = self.get_shortb_insertion_and_subescher_of_2_eschers(uv,self.concat(w,z, insertionpoint=core_w_z[-1]))
 
         if core_u_w[-1] == -1:
             core_uw_v = [-1,-1, -1]
             core_uw_z = [-1,-1, -1]
+            core_uw_vz = [-1,-1, -1]
         else:
             uw = self.concat(u, w, insertionpoint=core_u_w[-1])
             core_uw_v = self.get_shortb_insertion_and_subescher_of_2_eschers(uw,v)
             core_uw_z = self.get_shortb_insertion_and_subescher_of_2_eschers(uw,z)
+            if core_v_z[-1] == -1:
+                core_uw_vz = [-1, -1, -1]   
+            else:
+                core_uw_vz = self.get_shortb_insertion_and_subescher_of_2_eschers(uw,self.concat(v,z, insertionpoint=core_v_z[-1]))
 
         if core_v_w[-1] == -1:
             core_vw_u = [-1, -1, -1]
             core_vw_z = [-1, -1, -1]
+            core_vw_uz = [-1, -1, -1]
         
         else:
             vw = self.concat(v, w, insertionpoint=core_v_w[-1])
             core_vw_u = self.get_shortb_insertion_and_subescher_of_2_eschers(vw,u)
             core_vw_z = self.get_shortb_insertion_and_subescher_of_2_eschers(vw,z)
-
+            if core_u_z[-1] == -1:
+                core_vw_uz = [-1, -1, -1]
+            else:
+                core_vw_uz = self.get_shortb_insertion_and_subescher_of_2_eschers(vw,self.concat(u,z, insertionpoint=core_u_z[-1]))
+        
         if core_u_z[-1] == -1:
             core_uz_w = [-1, -1, -1]
             core_uz_v = [-1, -1, -1]
@@ -121,11 +157,65 @@ class EscherCoreGeneratorQuadruple(EscherCoreGeneratorAbstract):
             core_wz_u = self.get_shortb_insertion_and_subescher_of_2_eschers(wz,u)
             core_wz_v = self.get_shortb_insertion_and_subescher_of_2_eschers(wz,v)
 
+        #Calculate the core of a quadruple pair of type abc_d
+
+        if core_uv_w[-1] == -1 and core_uw_v[-1] == -1 and core_vw_u[-1] == -1:
+            core_uvw_z = [-1, -1, -1]
+        else:
+            if core_uv_w[-1] != -1: 
+                uvw = self.concat(uv, w, insertionpoint=core_uv_w[-1])
+                core_uvw_z = self.get_shortb_insertion_and_subescher_of_2_eschers(uvw,z)
+            elif core_uw_v[-1] != -1:
+                uwv = self.concat(uw, v, insertionpoint=core_uw_v[-1])
+                core_uvw_z = self.get_shortb_insertion_and_subescher_of_2_eschers(uwv,z)
+            elif core_vw_u[-1] != -1:
+                vwu = self.concat(vw, u, insertionpoint=core_vw_u[-1])
+                core_uvw_z = self.get_shortb_insertion_and_subescher_of_2_eschers(vwu,z)  
+
+        if core_uv_z[-1] == -1 and core_uz_v[-1] == -1 and core_vz_u[-1] == -1:
+            core_uvz_w = [-1, -1, -1]
+        else:
+            if core_uv_z[-1] != -1:
+                uvz = self.concat(uv, z, insertionpoint=core_uv_z[-1])
+                core_uvz_w = self.get_shortb_insertion_and_subescher_of_2_eschers(uvz,w)
+            elif core_uz_v[-1] != -1:
+                uzv = self.concat(uz, v, insertionpoint=core_uz_v[-1])
+                core_uvz_w = self.get_shortb_insertion_and_subescher_of_2_eschers(uzv,w)
+            elif core_vz_u[-1] != -1:
+                vzu = self.concat(vz, u, insertionpoint=core_vz_u[-1])
+                core_uvz_w = self.get_shortb_insertion_and_subescher_of_2_eschers(vzu,w)
+
+        if core_uw_z[-1] == -1 and core_uz_w[-1] == -1 and core_wz_u[-1] == -1:
+            core_uwz_v = [-1, -1, -1]   
+        else:
+            if core_uw_z[-1] != -1:
+                uwz = self.concat(uw, z, insertionpoint=core_uw_z[-1])
+                core_uwz_v = self.get_shortb_insertion_and_subescher_of_2_eschers(uwz,v)
+            elif core_uz_w[-1] != -1:
+                uzw = self.concat(uz, w, insertionpoint=core_uz_w[-1])
+                core_uwz_v = self.get_shortb_insertion_and_subescher_of_2_eschers(uzw,v)
+            elif core_wz_u[-1]  != -1:
+                wzu = self.concat(wz, u, insertionpoint=core_wz_u[-1])
+                core_uwz_v = self.get_shortb_insertion_and_subescher_of_2_eschers(wzu,v)
+
+        if core_vw_z[-1] == -1 and core_vz_w[-1] == -1 and core_wz_v[-1] == -1:
+            core_vwz_u = [-1, -1, -1]
+
+        else:
+            if core_vw_z[-1] != -1:
+                vwz = self.concat(vw, z, insertionpoint=core_vw_z[-1])
+                core_vwz_u = self.get_shortb_insertion_and_subescher_of_2_eschers(vwz,u)
+            elif core_vz_w[-1] != -1:
+                vzw = self.concat(vz, w, insertionpoint=core_vz_w[-1])
+                core_vwz_u = self.get_shortb_insertion_and_subescher_of_2_eschers(vzw,u)
+            elif core_wz_v[-1]  != -1:
+                wzv = self.concat(wz, v, insertionpoint=core_wz_v[-1])
+                core_vwz_u = self.get_shortb_insertion_and_subescher_of_2_eschers(wzv,u)
 
 
         #print([0] + add_half(core_v_w) + add_half(core_uv_w) + add_half(core_uw_v))
         #print([0] + core_v_w + core_uv_w + core_uw_v)
-        return [0] + add_half(core_u_v) + add_half(core_v_w) + add_half(core_u_w) + add_half(core_uv_w) + add_half(core_uw_v) + add_half(core_vw_u)+add_half(core_vw_z) + add_half(core_u_z) + add_half(core_v_z) + add_half(core_w_z) + add_half(core_uv_z) + add_half(core_uw_z) + add_half(core_uz_w) + add_half(core_uz_v) + add_half(core_vz_w) + add_half(core_vz_u) + add_half(core_wz_u) + add_half(core_wz_v)
+        return [0] + add_half(core_u_v) + add_half(core_v_w) + add_half(core_u_w) + add_half(core_u_z) + add_half(core_v_z) + add_half(core_w_z) +add_half(core_uv_w) + add_half(core_uw_v) +add_half(core_vw_u)+add_half(core_vw_z) + add_half(core_uv_z) + add_half(core_uw_z) + add_half(core_uz_w) + add_half(core_uz_v) + add_half(core_vz_w) + add_half(core_vz_u) + add_half(core_wz_u) + add_half(core_wz_v)+add_half(core_uv_wz) + add_half(core_uw_vz) + add_half(core_vw_uz) + add_half(core_uvw_z) + add_half(core_uvz_w) + add_half(core_uwz_v)+add_half(core_vwz_u)
 
 
 
