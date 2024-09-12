151ba3b226cc9ddb7b7c87aae9f2c0549cba8a83
au596283 <gergely.berczi@gmail.com>
2024-09-12 20:47:50 +0200

    Minor changes

--- SPC/Restructure/main.py
+++ SPC/Restructure/main.py
@@ -83,17 +83,17 @@ def main(partition, training_data_load_path, model_load_path, model_save_path, m
 if __name__ == "__main__":
 
      # parameters
-    partition = (3,2,1,1) 
+    partition = (3,3,1,1) 
     training_data_load_path = "" # "SPC/Saves,Tests/Trainingdata/partition_5_4__5_core_9_7_2024.bin"
     training_data_save_path = "" #"SPC/Saves,Tests/Trainingdata/partition_4_3_2_3rows.bin" # "SPC/Saves,Tests/Trainingdata/partition_5_4__5_core.bin"
-    model_load_path =  "" # "SPC/Saves,Tests/models/Quadruple6.keras" # "SPC/Saves,Tests/models/for_result_viewer_test.keras" #"SPC/Saves,Tests/models/for_result_viewer_test.keras" # "SPC/Saves,Tests/models/tripple_escher.keras"#"" #"SPC/Saves,Tests/models/my_newmodel.keras"
-    model_save_path = "SPC/Saves,Tests/models/Quadruple9.keras"
-    model_save_time = 1 # how many seconds have to have elapsed before saving
+    model_load_path =  "" # "SPC/Saves,Tests/models/for_result_viewer_test.keras" #"SPC/Saves,Tests/models/for_result_viewer_test.keras" # "SPC/Saves,Tests/models/tripple_escher.keras"#"" #"SPC/Saves,Tests/models/my_newmodel.keras"
+    model_save_path = "SPC/Saves,Tests/models/testmodel_.keras"
+    model_save_time = 600 # how many seconds have to have elapsed before saving
     ml_training_algorithm_type = "RLAlgorithm" # exact name of the algorithm python class BruteForceAlgorithm or RLAlgorithm
-    ml_model_type =  "RLNNModel_Escher_Tripple" # RLNNModel_Escher RLNNModel_CorrectSequence or RLNNModel_Escher or RLNNModel_Escher_Tripple - exact name of the model python class. The model is the component that contains the weights and perform computations, but the algorithm decides how the model is used
-    core_generator_type =  "EscherCoreGeneratorQuadruple" # "EscherCoreGeneratorQuadruple "EscherCoreGeneratorBasic"   "EscherCoreGeneratorTripple" "EscherCoreGeneratorTrippleSymmetricNoEqual"
-    iteration_steps = 100
-    RL_n_graphs = 400
+    ml_model_type =  "RLNNModel_Escher_TrippleNoEqual" #RLNNModel_Escher_Tripple" # RLNNModel_Escher RLNNModel_CorrectSequence or RLNNModel_Escher or RLNNModel_Escher_Tripple - exact name of the model python class. The model is the component that contains the weights and perform computations, but the algorithm decides how the model is used
+    core_generator_type =  "EscherCoreGeneratorQuadruple" #"EscherCoreGeneratorTrippleSymmetricNoEqual" # "EscherCoreGeneratorBasic"   "EscherCoreGeneratorTripple" "EscherCoreGeneratorTrippleSymmetricNoEqual"
+    iteration_steps = 500
+    RL_n_graphs = 200
     condition_rows = 4
     plot_after_training = True
 
